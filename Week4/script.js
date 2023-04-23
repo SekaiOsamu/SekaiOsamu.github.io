ef5733a1ba516d6eb7c34d916542475fdfcd37b7
SekaiOsamu <130111759+SekaiOsamu@users.noreply.github.com>
2023-04-23 18:32:02 +0100

    updated

---
+++ Week4/script.js
@@ -0,0 +1,22 @@
+var names=new Array();
+names[0]="Sekai";
+names[1]="Mayra";
+names[2]="Rosa";
+names[3]="Dazai";
+names[4]="Morytski";
+names[5]="Mooryana";
+names[6]="Jleena";
+names[7]="Morganaa";
+names[8]="Jordania";
+names[9]="Fyodor";
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
+		console.log("GoodBye" + names[i])
+	}
+	else{
+		console.log("Hello " + names[i])
+	}
+
+}
+ 
